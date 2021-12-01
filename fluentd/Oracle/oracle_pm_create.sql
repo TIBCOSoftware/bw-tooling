4d517f2a588520d8d281170e451bf6755c0851a6
Aditya Anil Poduval <apoduval@tibco.com>
2021-12-01 09:26:32 +0530

    Added the Create table query for ActivityLogging stats with size 32767

--- fluentd/Oracle/oracle_pm_create.sql
+++ fluentd/Oracle/oracle_pm_create.sql
@@ -93,3 +93,40 @@ EXCEPTION
       RAISE;
     END IF;
 END;
+
+
+/*
+--
+-- Table structure for table activityloggingstats
+-- Use this table if the MAX_STRING_SIZE of Oracle DB is EXTENDED
+--
+BEGIN
+  EXECUTE IMMEDIATE 'CREATE TABLE activityloggingstats (
+    activityinstanceuid varchar(255) NOT NULL,
+    timestmp NUMBER(19,0) NULL,
+    applicationname varchar(255) NULL,
+    applicationversion varchar(255) NULL,
+    modulename varchar(255) NULL,
+    moduleversion varchar(255) NULL,
+    processname varchar(255) NULL,
+    processinstanceid varchar(255) NULL,
+    activityname varchar(255) NULL,
+    activitystarttime NUMBER(19,0) NULL,
+    activitydurationtime NUMBER(19,0) NULL,
+    activityevaltime NUMBER(19,0) NULL,
+    activitystate varchar(255) NULL,
+    domainname varchar(255) NULL,
+    appspacename varchar(255) NULL,
+    appnodename varchar(255) NULL,
+    activityinput varchar(32767) NULL,
+    activityoutput varchar(32767) NULL,
+    activityexecutionid varchar(255) NULL,
+    PRIMARY KEY (activityinstanceuid)
+)' ;
+EXCEPTION
+  WHEN OTHERS THEN
+    IF SQLCODE != -955 THEN
+      RAISE;
+    END IF;
+END;
+*/
